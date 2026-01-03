1bdfa717733c0e148b9f1438da62b703c700fb92
no-fluke <berlin14151617@gmail.com>
2026-01-03 08:38:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,8 @@ API_HASH = os.environ.get("API_HASH", "")
 ADMINS = [int(id) for id in os.environ.get("ADMINS", "").split(",")]
 DB_URI = os.environ.get("DB_URI", "")
 DB_NAME = os.environ.get("DB_NAME", "REXSaveRestricted")
-LOG_CHANNEL = 
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", 0))
+
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
 KEEP_ALIVE_URL = os.environ.get("KEEP_ALIVE_URL", "")
 # Rexbots
